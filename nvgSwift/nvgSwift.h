1e833541c7ec53072de8836f193606231ec62430
Michael Oneppo <moneppo@gmail.com>
2016-04-22 22:05:24 -0700

    Initial Commit

---
+++ nvgSwift/nvgSwift.h
@@ -0,0 +1,19 @@
+//
+//  nvgSwift.h
+//  nvgSwift
+//
+//  Created by Michael Oneppo on 4/22/16.
+//  Copyright © 2016 moneppo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for nvgSwift.
+FOUNDATION_EXPORT double nvgSwiftVersionNumber;
+
+//! Project version string for nvgSwift.
+FOUNDATION_EXPORT const unsigned char nvgSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <nvgSwift/PublicHeader.h>
+
+
